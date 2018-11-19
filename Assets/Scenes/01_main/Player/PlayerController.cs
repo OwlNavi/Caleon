52eb311cbcdbc9c50e729086d4af39512d0125f3
Craig <cwagthomas@gmail.com>
2018-11-20 01:01:32 +1300

    Initial Commit Pushing 3d cylinders

---
+++ Assets/Scenes/01_main/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 10;
+    public float rotateSpeed = 10;
+    public Camera viewCamera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * -                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+        */
+
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("LeftHorizontal"), 0, Input.GetAxis("LeftVertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed * Time.deltaTime;
+        transform.Translate(moveVelocity);
+        transform.Rotate(0, Input.GetAxis("RightHorizontal"), 0);
+       
+    }
+}
---
+++ Assets/Scenes/01_main/Player/ThirdPersonCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 75.0f;
+    private float distance = 5.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 1.0f;
+    private float sensitivityY = 10.0f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentX += Input.GetAxis("RightHorizontal");
+        currentY += Input.GetAxis("RightVertical");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+	}
+
+    private void LateUpdate()
+    {
+        //calculate posisiton after player moves
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
